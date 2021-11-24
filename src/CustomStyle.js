36d103ae5df057bca438e339182dd7a5d988763e
wobbegongNFT <ollierazdow@protonmail.com>
2021-11-23 20:43:00 -0500

    minor final changes

--- src/CustomStyle.js
+++ src/CustomStyle.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 import { Glview } from "./modules/glview.js";
 import prog from "./zemm.js";
-// import * as dat from "./modules/dat.gui.module.min.js";
 import MT from './mersenne.js';
 import{name_select, enumeration} from './namegen.js';
 const {min, max, abs, round, floor} = Math;
@@ -110,30 +109,34 @@ function block_handler(prog, block){
 	let a = round(v1*(prog.etc.texlen_a-1));
 	let b = round(v2*(prog.etc.texlen_b-1));
 	let c = round(999*v1*v2%(prog.etc.texlen_a-1));
-	// let c = floor(hash11(6.*(3.3+a+b))*11.);
 
 	prog.uniforms.idx = a;
 	prog.uniforms.idx2 = b;
 	prog.uniforms.idxr = c;
 	prog.uniforms.offs = v1;
-	let n = min(lerp(v2, .45, 1.1), 1);
+	let n = min(lerp(v2, .5, 1.1), 1);
  	prog.uniforms._div =  n;
  	prog.uniforms.sat = v1*.4;
  	prog.uniforms.cont = v2*.1;
 
     // doublemage, expand, ripple
- 	let weights = [.088, .22, .04];  //88, 220, 40 per 1000
+    //99, 300, 40 per 1000
+ 	let weights = [.099, .3, .04];  
  	let rare = rare_handler(prog, v1, weights, (p)=>{
  		if(p.uniforms.idx == 8 && p.uniforms.idx2 == 7 ){
  			prog.uniforms._oscmixm = 1;
  		}
  	});
+
  	let name = name_select(v1);
  	let en = enumeration(v1);
  	glob.attributes = genAttributes(prog, name, en);
- 	let _i = window.blabel ? window.blabel.innerHTML : '';
- 	console.log(_i, name, rare);
- 	console.log(glob.attributes);
+
+ 	if(prog.print){
+	 	let _i = prog.blabel ? prog.blabel.innerHTML : '';
+	 	console.log(_i, name, rare);
+	 	console.log(glob.attributes);
+ 	}
 }
 
 function rare_handler(prog, r, weights, uniformrule){
@@ -146,7 +149,7 @@ function rare_handler(prog, r, weights, uniformrule){
 }
 
 function mod_handler(prog, mod1, mod2, mod3, mod4, mod5, mod6){
-	let t = lerp(mod1, .02, 1.);
+	let t = lerp(mod1, .026, 1.);
 	let a = piecewise(t, [0,.49,.6,.67,.718,.883,1],[1,.99,.9,.322,.144,.008,0]); 
 	let b = piecewise(t, [0,.482,.606,.7,.846,.92,1],[.05,.79,.514,.3,.395,.554,.936]); 
 	prog.uniforms.select_lev = a;
@@ -154,7 +157,7 @@ function mod_handler(prog, mod1, mod2, mod3, mod4, mod5, mod6){
 	prog.uniforms._rlev = lerp(t, .2, .6);
 	prog.uniforms._oscmixr = mod2;
 	prog.uniforms._sdf = mod3*.05;
-	prog.uniforms.texmix = min(max(.23, mod4), .77);
+	prog.uniforms.texmix = lerp(mod4, .32, .68);
 	prog.uniforms.offs_fine = 0; 
 }
 
@@ -171,30 +174,26 @@ function addGUI(glview, parentel){
 }
 
 function useAttributes(ref) {
-  // Update custom attributes related to style when the modifiers change
-  useEffect(() => {
-    ref.current = () => {
-      return {
-        // This is called when the final image is generated, when creator opens the Mint NFT modal.
-        // should return an object structured following opensea/enjin metadata spec for attributes/properties
-        // https://docs.opensea.io/docs/metadata-standards
-        // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1155.md#erc-1155-metadata-uri-json-schema
-        attributes : glob.attributes
-      };
-    };
-  }, [ref]);
+	useEffect(() => {
+		ref.current = () => {
+			return {
+				// This is called when the final image is generated, when creator opens the Mint NFT modal.
+				// https://docs.opensea.io/docs/metadata-standards
+				attributes : glob.attributes
+		};
+	};
+	}, [ref]);
 }
 
 const Display = ({canvasRef, block, width, height, animate, mod1, mod2, mod3, mod4, attributesRef, handleResize, ...props}) =>{
-
 	useAttributes(attributesRef);
 
 	//init
 	useEffect(() => {
 		glob.glview = new Glview(canvasRef.current, prog);
-		// addGUI(glob.glview, document.querySelector('#root'));
-		window.sceneref = glob.glview.programs[0];
-		window.blabel = document.querySelector('.value-label');
+		window.sceneprog = prog;
+		prog.print = false;
+		prog.blabel = document.querySelector('.value-label');
 		return ()=>{
 			if(glob.glview){glob.glview.switchPogram(-1);}
 		}
